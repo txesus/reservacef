fce443981b7e7a2e3b927c6bc5eb770e40d413f2
Jesus Esteban <jesusestebansanchez@gmail.com>
2024-03-21 00:41:36 +0100

    form

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,93 +1,61 @@
 <template>
   <v-container>
     <v-row class="text-center">
+
       <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to the Vuetify 3 Beta
+          Formulario de la CEF
         </h1>
-
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
+      </v-col>
+      <v-col cols="12">
+        <v-form @submit.prevent>
+        <v-col sm="6">
+          <v-text-field
+            v-model="firstName"
+            :rules="rules"
+            label="Nombre"
+          ></v-text-field>
+        </v-col>
+        <v-col sm="6">
+          <v-text-field
+            v-model="firstName"
+            :rules="rules"
+            label="Apellidos"
+          ></v-text-field>
+        </v-col>
+        <v-col sm="6">
+          <v-text-field
+            v-model="firstName"
+            :rules="rules"
+            label="Teléfono"
+          ></v-text-field>
+        </v-col>
+        <v-col sm="6">
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            hide-details
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col sm="6">
+          <v-date-picker></v-date-picker>
+        </v-col>
+        <v-col sm="6">
+          <v-select
+            label="Dias que necesitas hotel"
+            :items="['Jueves', 'Viernes', 'Sábado']"
+            multiple
+            variant="outlined"
+          ></v-select>
+        </v-col>
+        <v-btn class="mt-2" type="submit" block>Enviar</v-btn>
+      </v-form>
+      
       </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          What's next?
-        </h2>
 
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
     </v-row>
   </v-container>
 </template>
